6da739223fa10acf20060c260235d765fe845cd9
bernardkroes <bernard@shiftf5.nl>
2020-12-05 10:00:23 +0100

    shortened it a bit

--- day_5.rb
+++ day_5.rb
@@ -1,11 +1,19 @@
-def boardingpass_to_seat_id(line)
+def first_attempt_for_boardingpass_to_seat_id(line)
   row, col = 0, 0
+
   0.upto(6) do |i|
     row += (line[i] == "B" ? 2**(6-i) : 0)
   end
   0.upto(2) do |i|
     col += (line[7+i] == "R" ? 2**(2-i) : 0)
   end
+  row * 8 + col
+end
+
+# shorter, using to_i and base 2
+def boardingpass_to_seat_id(line)
+  row = line[0..6].gsub("B", "1").gsub("F","0").to_i(2)
+  col = line[7..9].gsub("R", "1").gsub("L","0").to_i(2)
   row * 8 + col
 end
 
