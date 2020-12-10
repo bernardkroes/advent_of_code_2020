6ed20430e24d5c1ca57f9f769155e238bc665933
bernardkroes <bernard@shiftf5.nl>
2020-12-10 11:43:59 +0100

    added dp and benchmarking

--- day_10.rb
+++ day_10.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 all_jolts = File.read('day_10_input.txt').split("\n").map(&:to_i).sort
 
 all_delta_jolts = [all_jolts[0]]
@@ -15,17 +17,41 @@ all_jolts.each do |j|
 end
 
 def count_paths(graph, src, dest, cache = {})
-  cache_hashkey = "#{src}_#{dest}"
+  return 1 if src == dest
+  return cache[src] if cache.has_key?(src)
 
-  if not cache.has_key?(cache_hashkey)
-    return 1 if src == dest
-    the_count = 0
-    graph[src].each do |e|
-      the_count += count_paths(graph, e, dest, cache)
-    end
-    cache[cache_hashkey] = the_count
+  the_count = 0
+  graph[src].each do |e|
+    the_count += count_paths(graph, e, dest, cache)
   end
-  return cache[cache_hashkey]
+  cache[src] = the_count
+  cache[src]
+end
+
+def count_using_dp(all_jolts, i, dest, cache = {})
+  return 1 if i == dest
+  return cache[i] if cache.has_key?(i)
+
+  the_count = 0
+  (i+1).upto(i+3) do |j|
+    if all_jolts.include?(j)
+      the_count += count_using_dp(all_jolts, j, dest, cache)
+    end
+  end
+  cache[i] = the_count
+  cache[i]
+end
+
+
+Benchmark.bmbm do |x|
+  x.report(:graph) do
+    count_paths(graph, all_jolts[0], all_jolts[-1])
+  end
+  x.report(:dp) do
+    count_using_dp(all_jolts, all_jolts[0], all_jolts[-1])
+  end
 end
 
 puts count_paths(graph, all_jolts[0], all_jolts[-1])
+puts count_using_dp(all_jolts, all_jolts[0], all_jolts[-1])
+
