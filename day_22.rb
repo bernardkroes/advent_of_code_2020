b4f4d2645d05d280a81519c5399bf95d5648dcbb
bernardkroes <bernard@shiftf5.nl>
2020-12-22 15:05:06 +0100

    hash for cache, use push to extend arrays

--- day_22.rb
+++ day_22.rb
@@ -33,9 +33,9 @@ while (deck1.size > 0) && (deck2.size > 0)
   c1 = deck1.shift
   c2 = deck2.shift
   if c1 > c2
-    deck1 += [c1, c2]
+    deck1.push(c1, c2)
   elsif c2 > c1
-    deck2 += [c2, c1]
+    deck2.push(c2, c1)
   end
 end
 
@@ -49,9 +49,9 @@ deck1 = init_deck1.clone
 deck2 = init_deck2.clone
 
 def combat(deck1, deck2, games_seen) # return winner (1 or 2) or -1: no winner yet
-  return 1 if games_seen.any? { |g| g[0] == deck1 && g[1] == deck2 }
-
-  games_seen << [deck1.clone, deck2.clone]
+  hash_key = deck1.join("_") + ":" + deck2.join("_")
+  return 1 if games_seen.has_key?(hash_key)
+  games_seen[hash_key] = 1
 
   c1 = deck1.shift
   c2 = deck2.shift
@@ -61,7 +61,7 @@ def combat(deck1, deck2, games_seen) # return winner (1 or 2) or -1: no winner y
     sub_deck1 = deck1[0...(c1)].clone
     sub_deck2 = deck2[0...(c2)].clone
 
-    sub_games_seen = []
+    sub_games_seen = {}
     while card_winner < 0
       card_winner = combat(sub_deck1, sub_deck2, sub_games_seen)
     end
@@ -69,19 +69,20 @@ def combat(deck1, deck2, games_seen) # return winner (1 or 2) or -1: no winner y
     card_winner = c1 > c2 ? 1 : 2
   end
   if card_winner == 1
-    deck1 << c1
-    deck1 << c2
+    deck1.push(c1, c2)
   elsif card_winner == 2
-    deck2 << c2
-    deck2 << c1
+    deck2.push(c2, c1)
   end
-  return 2 if deck1.size == 0
-  return 1 if deck2.size == 0
-
+  if deck2.size == 0
+    return 1
+  end
+  if deck1.size == 0
+    return 2
+  end
   return -1
 end
 
-games_seen = []
+games_seen = {}
 the_winner = -1
 while the_winner < 0
   the_winner = combat(deck1, deck2, games_seen)
